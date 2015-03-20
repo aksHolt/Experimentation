f70f59c705cb2eab749a01f66d79b7c1007b046b
aksHolt <aleksholt@gmail.com>
2015-03-20 14:39:40 +0300

    new homework

---
+++ io_2_unsolved.py
@@ -0,0 +1,23 @@
+one = {1:"  *	",2:"  *	",3:"  *	",4:"  *	",5:"  *	",}
+two = {1:"  ****	 ", 2:" *	   *", 3:"	  *	", 4:"  *	", 5:" ******	"}
+three ={1:"	  ***   ", 2:"         *", 3:"        * ", 4:"         *", 5:"	  ***   "}
+
+numob = ['1','2','3']
+printob=[]
+#def count(numline):
+#     print()
+#    
+#    
+#    return
+#    
+#count(2)
+def conv():
+    inp = input("Enter 1,2 or 3 on random conditions:")
+    for a in inp:
+        numob.append(a)
+    for i in numob:
+        
+    return
+
+
+conv()
